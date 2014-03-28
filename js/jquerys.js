3f1e01b79f8a2de6bbccd93609e93eb481098823
EderVs04 <eedervs.97@hotmail.com>
2014-03-27 21:48:17 -0600

    Upss xD

--- js/jquerys.js
+++ js/jquerys.js
@@ -19,5 +19,15 @@ $(document).ready(function()
     });
   }
 );
+$(function()
+{
+  $('#slider div:gt(0)').hide();
+    setInterval(function()
+      {
+      $('#slider div:first-child').fadeOut(0)
+          .next('div').fadeIn(2000)
+          .end().appendTo('#slider');
+        } , 6000);
+});
 
  
