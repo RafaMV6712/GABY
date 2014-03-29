f40292278c86b5a0888fc0f7f20fc462c2824229
Rafael <fayo_10mex@hotmail.com>
2014-03-29 17:50:09 -0600

    Update classie.js

--- js/classie.js
+++ js/classie.js
@@ -1,43 +1,56 @@
-(function() {
-	function mobilecheck() {
-		var check = false;
-	}
-	function init() {
+( function( window ) {
 
-		var menu = document.getElementById( 'bt-menu' ),
-			trigger = menu.querySelector( 'a.bt-menu-trigger' ),
-	
-			eventtype = mobilecheck() ? 'touchstart' : 'click',
-			resetMenu = function() {
-				classie.remove( menu, 'bt-menu-open' );
-				classie.add( menu, 'bt-menu-close' );
-			},
-			closeClickFn = function( ev ) {
-				resetMenu();
-				overlay.removeEventListener( eventtype, closeClickFn );
-			};
+'use strict';
 
 
-		var overlay = document.createElement('div');
-		overlay.className = 'bt-overlay';
-		menu.appendChild( overlay );
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
+
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+var classie = {
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
 
 
-		trigger.addEventListener( eventtype, function( ev ) {
-			ev.stopPropagation();
-			ev.preventDefault();
-			
-			if( classie.has( menu, 'bt-menu-open' ) ) {
-				resetMenu();
-			}
-			else {
-				classie.remove( menu, 'bt-menu-close' );
-				classie.add( menu, 'bt-menu-open' );
-				overlay.addEventListener( eventtype, closeClickFn );
-			}
-		});
-	}
+if ( typeof define === 'function' && define.amd ) {
+  define( classie );
+} else {
+  window.classie = classie;
+}
 
-	init();
-
-})();
+})( window );
